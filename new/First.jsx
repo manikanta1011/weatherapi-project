785dfaf947904fc118e70decdfd6b309c4f4291c
Hareesh 981 <hareeshgiddanti@gmail.com>
2024-12-05 11:12:03 +0530

    changed the set error text from city to town

--- new/First.jsx
+++ new/First.jsx
@@ -11,7 +11,7 @@ function First() {
  
   const fetchWeather = () => {
     if (!city) {
-      setError("Please enter a city name.");
+      setError("Please enter a town name.");
       return;
     }
  
